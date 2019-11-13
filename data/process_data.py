0504088c28322c47cb2c5d4db7e934f7029b163a
Sepehr <sepehr.ramyar@gmail.com>
2019-11-12 18:35:12 -0800

    etl pipeline complete

--- data/process_data.py
+++ data/process_data.py
@@ -3,17 +3,75 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
+import sqlite3
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    INPUT - filepaths to the messages and categories data sets
+
+    OUTPUT - data frame
+
+    reads in, merges, and outputs a combined dataframe
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    df = messages.merge(categories, on = ['id'])
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    INPUT - dataframe
+
+    OUTPUT - cleaned and processed dataframe
+    '''
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand = True)
+
+    row = categories.loc[0]
+
+    # dropping the last two characters of each string/category name
+    category_colnames = [w[:-2] for w in row if ((w.endswith('-0')) | (w.endswith('-1')))]
+
+    categories.columns = category_colnames
+
+    # binary category conversion: set each value to be the last character of the string
+    for column in categories:
+        categories[column] = categories[column].apply(lambda x:
+         1 if x.endswith('-1') else 0)
+    
+    # replacing the old categories column with categories dataframe
+    df.drop(columns = ['categories'], inplace = True)
+    df = pd.concat([df,categories], axis = 1)
+
+    # removing duplicates
+    df.drop_duplicates(subset = ['id'], inplace = True)
+
+    return df
+
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    INPUT - claned dataframe (df) and database filename
+
+    OUTPUT - True (Success)/ False (Fail)
+
+    Saves the cleaned dataframe into database at the given path
+    '''  
+
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    try:
+        engine.execute('DROP TABLE IF EXISTS InsertTableName')
+        engine.execute('DROP TABLE IF EXISTS Message')
+        df.to_sql('Message', engine, index=False)
+        return True
+    except:
+        print('Failed to write dataframe to database')
+        return False
 
 
 def main():
